2b32506559445f04e2badebf4e09cc9119108ba2
Daniel McCain <dsmccain@acm.org>
2014-08-02 00:35:42 +0200

    Added tweet information function, which reply uses
    - The reply function no longer requires the user name
    
    - Also added the base uri "https://api.twitter.com/1.1"

--- Main.hs
+++ Main.hs
@@ -24,10 +24,12 @@ instance ToJSON User
 instance FromJSON Tweet
 instance ToJSON Tweet
 
+baseUri :: String
+baseUri = "https://api.twitter.com/1.1"
+
 mentions :: Credential -> IO (Either String [Tweet])
 mentions credential = do
-    request <- parseUrl $
-        "https://api.twitter.com/1.1/statuses/mentions_timeline.json?" ++
+    request <- parseUrl $ baseUri ++ "/statuses/mentions_timeline.json?" ++
                  "contributor_details=true"
     response <- withManager $ \m -> do
         -- OAuth authentication
@@ -36,9 +38,9 @@ mentions credential = do
         httpLbs signedRequest m
     return $ eitherDecode $ responseBody response
 
-tweet :: Credential -> String -> IO (Either String Tweet)
-tweet credential message = do
-    request <- parseUrl "https://api.twitter.com/1.1/statuses/update.json"
+sendTweet :: Credential -> String -> IO (Either String Tweet)
+sendTweet credential message = do
+    request <- parseUrl $ baseUri ++ "/statuses/update.json"
     let update = pack message
         postData = [("status", update)] :: [(ByteString, ByteString)]
         request' = urlEncodedBody postData request
@@ -49,24 +51,40 @@ tweet credential message = do
         httpLbs signedRequest m
     return $ eitherDecode $ responseBody response
 
-replyTweet :: Credential
-           -> Int    -- Status ID that the reply is directed to
-           -> String -- User name of the person that the reply is directed to
-           -> String -- Message to send as a reply
-           -> IO (Either String Tweet)
-replyTweet credential statusID username message = do
-    request <- parseUrl "https://api.twitter.com/1.1/statuses/update.json"
-    let update = pack $ '@' : username ++ " " ++ message
-        postData = [ ("status", update)
-                   , ("in_reply_to_status_id", pack $ show statusID)
-                   ] :: [(ByteString, ByteString)]
-        request' = urlEncodedBody postData request
+tweetInfo :: Credential
+          -> Int    -- ID of the tweet the information is wanted about
+          -> IO (Either String Tweet)
+tweetInfo credential tweetID = do
+    request <- parseUrl $ baseUri ++ "/statuses/show.json?id=" ++ show tweetID
     response <- withManager $ \m -> do
         -- OAuth authentication
-        signedRequest <- signOAuth myOAuth credential request'
+        signedRequest <- signOAuth myOAuth credential request
         -- Send request
         httpLbs signedRequest m
     return $ eitherDecode $ responseBody response
+
+replyTweet :: Credential
+           -> Int    -- Tweet ID that the reply is directed to
+           -> String -- Message to send as a reply
+           -> IO (Either String Tweet)
+replyTweet credential tweetID message = do
+    info <- tweetInfo credential tweetID
+    case info of
+         Right info' -> do
+             request <- parseUrl $ baseUri ++ "/statuses/update.json"
+             let username = (unpack . screen_name . user) info'
+                 update = pack $ '@' : username ++ " " ++ message
+                 postData = [ ("status", update)
+                            , ("in_reply_to_status_id", pack $ show tweetID)
+                            ] :: [(ByteString, ByteString)]
+                 request' = urlEncodedBody postData request
+             response <- withManager $ \m -> do
+                 -- OAuth authentication
+                 signedRequest <- signOAuth myOAuth credential request'
+                 -- Send request
+                 httpLbs signedRequest m
+             return $ eitherDecode $ responseBody response
+         _ -> return info
 
 main :: IO ()
 main = do
@@ -82,12 +100,12 @@ main = do
              let mLatest = listToMaybe m -- Last person to mention the user
              case mLatest of
                   Nothing -> putStrLn "No one has mentioned me!"
-                  Just tweetInfo -> do
-                      let username = (unpack . screen_name . user) tweetInfo
-                          tweetID = Main.id tweetInfo
-                          msg = "this is my autoreply!"
+                  Just tweet -> do
+                      let username = (unpack . screen_name . user) tweet
+                          tweetID = Main.id tweet
+                          msg = "this is another autoreply!!"
                       putStrLn $ "Sending a reply to " ++ username ++ "!"
-                      eReply <- replyTweet accessToken tweetID username msg
+                      eReply <- replyTweet accessToken tweetID msg
                       print eReply
 
 {-# ANN module ("HLint: ignore Use camelCase" :: String) #-}
