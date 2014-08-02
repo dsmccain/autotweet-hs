c2b9b8dcee0603648a70a41e394b2281558b133b
Daniel McCain <dsmccain@acm.org>
2014-08-02 15:44:12 +0200

    Use of ReaderT Monad Transformer in actions module

--- Main.hs
+++ Main.hs
@@ -1,27 +1,37 @@
+import Control.Monad.IO.Class (liftIO)
+import Control.Monad.Reader (ReaderT, runReaderT)
 import Data.Maybe (listToMaybe)
 import Data.Text (unpack)
 import Twitter.Action
 import Twitter.OAuth
+import Web.Authenticate.OAuth (Credential)
 
-main :: IO ()
-main = do
-    accessToken <- login
-    eMentions <- mentions accessToken
+-- Autorespond to the last person that mentioned the user
+-- Tweets if that hasn't happened
+autorespondToFirstMention :: ReaderT Credential IO ()
+autorespondToFirstMention = do
+    eMentions <- mentions
     case eMentions of
          Left err -> do
-             putStrLn "There was an error when getting the mentions"
-             putStrLn err
+             liftIO $ putStrLn "There was an error when getting the mentions"
+             liftIO $ putStrLn err
          Right m  -> do
              let mLatest = listToMaybe m -- Last person to mention the user
              case mLatest of
                   Nothing -> do
-                      putStrLn "No one has mentioned me! Gotta let the world know"
-                      eTweet <- sendTweet accessToken "No one has mentioned me!!"
-                      print eTweet
+                      liftIO $ putStrLn $ "No one has mentioned me! " ++
+                          "Gotta let the world know"
+                      eTweet <- sendTweet "No one has mentioned me!!"
+                      liftIO $ print eTweet
                   Just tweet -> do
                       let username = (unpack . screen_name . user) tweet
                           tweetID = Twitter.Action.id tweet
                           msg = "oh yeah?!"
-                      putStrLn $ "Sending a reply to " ++ username ++ "!"
-                      eReply <- replyTweet accessToken tweetID msg
-                      print eReply
+                      liftIO $ putStrLn $ "Replying to " ++ username ++ "!"
+                      eReply <- replyTweet tweetID msg
+                      liftIO $ print eReply
+
+main :: IO ()
+main = do
+    accessToken <- login
+    runReaderT autorespondToFirstMention accessToken
--- Twitter/Action.hs
+++ Twitter/Action.hs
@@ -2,7 +2,7 @@
 
 module Twitter.Action where
 import Control.Monad.Catch (MonadThrow)
-import Control.Monad.IO.Class (MonadIO)
+import Control.Monad.Reader
 import Control.Monad.Trans.Control (MonadBaseControl)
 import Data.Aeson
 import Data.ByteString.Char8 (ByteString, pack)
@@ -30,18 +30,11 @@ instance ToJSON Tweet
 baseUri :: String
 baseUri = "https://api.twitter.com/1.1"
 
-{-
-    The type constraints for twitterGETreq and twitterPOSTreq come from
-    the following functions:
-        parseUrl :: MonadThrow m => String -> m Request
-        withManager :: (MonadIO m, MonadBaseControl IO m) =>
-                       (Manager -> ResourceT m a) -> m a
-        eitherDecode :: FromJSON a => ByteString -> Either String a
--}
 twitterPOSTreq :: (FromJSON a, MonadThrow m, MonadIO m, MonadBaseControl IO m) =>
-                  Credential -> String ->
-                  [(ByteString, ByteString)] -> m (Either String a)
-twitterPOSTreq credential req postData = do
+                  String -> [(ByteString, ByteString)] ->
+                  ReaderT Credential m (Either String a)
+twitterPOSTreq req postData = do
+    credential <- ask
     request <- parseUrl $ baseUri ++ req
     -- Change the request method to POST if postData is not empty
     let request' = if null postData
@@ -55,31 +48,28 @@ twitterPOSTreq credential req postData = do
     return $ eitherDecode $ responseBody response
 
 twitterGETreq :: (FromJSON a, MonadThrow m, MonadIO m, MonadBaseControl IO m) =>
-                 Credential -> String -> m (Either String a)
-twitterGETreq credential req = twitterPOSTreq credential req []
+                 String -> ReaderT Credential m (Either String a)
+twitterGETreq req = twitterPOSTreq req []
 
-mentions :: Credential -> IO (Either String [Tweet])
-mentions credential = twitterGETreq credential request
+mentions :: ReaderT Credential IO (Either String [Tweet])
+mentions = twitterGETreq request
   where request = "/statuses/mentions_timeline.json?contributor_details=true"
 
-sendTweet :: Credential -> String -> IO (Either String Tweet)
-sendTweet credential message = twitterPOSTreq credential request postData
-  where
-      postData = [("status", pack message)] :: [(ByteString, ByteString)]
-      request = "/statuses/update.json"
+sendTweet :: String -> ReaderT Credential IO (Either String Tweet)
+sendTweet message = twitterPOSTreq request postData
+  where postData = [("status", pack message)] :: [(ByteString, ByteString)]
+        request = "/statuses/update.json"
 
-tweetInfo :: Credential
-          -> Int    -- ID of the tweet the information is wanted about
-          -> IO (Either String Tweet)
-tweetInfo credential tweetID = twitterGETreq credential request
+tweetInfo :: Int -- ID of the tweet the information is wanted about
+          -> ReaderT Credential IO (Either String Tweet)
+tweetInfo tweetID = twitterGETreq request
   where request = "/statuses/show.json?id=" ++ show tweetID
 
-replyTweet :: Credential
-           -> Int    -- Tweet ID that the reply is directed to
+replyTweet :: Int    -- Tweet ID that the reply is directed to
            -> String -- Message to send as a reply
-           -> IO (Either String Tweet)
-replyTweet credential tweetID message = do
-    info <- tweetInfo credential tweetID
+           -> ReaderT Credential IO (Either String Tweet)
+replyTweet tweetID message = do
+    info <- tweetInfo tweetID
     case info of
          Right info' -> do
              let request = "/statuses/update.json"
@@ -88,7 +78,7 @@ replyTweet credential tweetID message = do
                  postData = [ ("status", update)
                             , ("in_reply_to_status_id", pack $ show tweetID)
                             ] :: [(ByteString, ByteString)]
-             twitterPOSTreq credential request postData
+             twitterPOSTreq request postData
          _ -> return info
 
 {-# ANN module ("HLint: ignore Use camelCase" :: String) #-}
